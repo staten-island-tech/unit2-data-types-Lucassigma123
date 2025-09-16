002c3067f8404e4efdad471a9023072fd8dfb5e0
Lucassigma123 <lucass126@nycstudents.net>
2025-09-16 10:16:29 -0400

    Update .py

--- .py
+++ .py
@@ -1,3 +1,10 @@
-sentence = input("write a sentence? ")
+ sentence = input("write a sentence")
 words = sentence.split # split separates the words in a sentence
-print(len(words)) # len counts the letter or words
+print (len(words))# len counts the letter or words 
+
+
+x = input("write a number")
+even = (x % 2)== 0
+if even:
+    print("even")
+else: print("odd")
