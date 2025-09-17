d7666babc24759fedfabf43cf09d3aa6c0ac0b86
Lucassigma123 <lucass126@nycstudents.net>
2025-09-17 10:14:52 -0400

    Update .py

--- .py
+++ .py
@@ -3,8 +3,12 @@ words = sentence.split() # split breaks the sentence into separate words
 print(len(words)) #len counts the words or letters 
 
 
-x = (int(input("write a number")))
+x = (int(input("write a number"))) #int makes it a integer
 even = (x % 2)== 0
 if even:
     print("even")
 else: print("odd")
+
+x= (input("The bill will be 100$ how was the service"))
+if input ("bad"):
+     print("0 tip")
