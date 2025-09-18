856fea2c370a716546100c831613f4c42e09eec9
Lucassigma123 <lucass126@nycstudents.net>
2025-09-18 10:14:08 -0400

    Update .py

--- .py
+++ .py
@@ -1,20 +1,27 @@
-sentence = input("Write a sentence") #asks person to write a sentence
-words = sentence.split() # split breaks the sentence into separate words
-print(len(words)) #len counts the words or letters 
+# sentence = input("Write a sentence") #asks person to write a sentence
+# words = sentence.split() # split breaks the sentence into separate words
+# print(len(words)) #len counts the words or letters 
 
 
-x = (int(input("write a number"))) #int makes it a integer
-even = (x % 2)== 0
-if even:
-    print("even")
-else: print("odd")
+# x = (int(input("write a number"))) #int makes it a integer
+# even = (x % 2)== 0
+# if even:
+#     print("even")
+# else: print("odd")
 
-x= input("The bill will be 100$ how was the service")
-if x =="bad":
-     print ("0 tip")
-if x=="okay":
-     print("15% tip")
-if x=="good":
-     print("20% tip")
-if x=="great" :
-     print("25% tip")
+# x= input("The bill will be 100$ how was the service")
+# if x =="bad":
+#      print ("0 tip")
+# if x=="okay":
+#      print("15% tip")
+# if x=="good":
+#      print("20% tip")
+# if x=="great" :
+#      print("25% tip")
+
+x= (int(input("write a number")))
+def factors (x,y):
+    f= x%y==0
+    print(f)
+
+
